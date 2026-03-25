e537b04c4525c59a0a8846734ae68198822092e2
Nikita Rudenko <rudenkornk@gmail.com>
2026-03-25 14:49:45 +0300

    feat(actions): add GoToNextWord & GoToPrevWord actions

--- television/television.rs
+++ television/television.rs
@@ -586,6 +586,8 @@ impl Television {
                     | Action::DeleteNextChar
                     | Action::GoToPrevChar
                     | Action::GoToNextChar
+                    | Action::GoToPrevWord
+                    | Action::GoToNextWord
                     | Action::GoToInputStart
                     | Action::GoToInputEnd
                     | Action::ToggleSelectionDown
@@ -946,7 +948,9 @@ impl Television {
             | Action::GoToInputEnd
             | Action::GoToInputStart
             | Action::GoToNextChar
-            | Action::GoToPrevChar => {
+            | Action::GoToPrevChar
+            | Action::GoToNextWord
+            | Action::GoToPrevWord => {
                 self.handle_input_action(action);
             }
             Action::SelectNextEntry => {
